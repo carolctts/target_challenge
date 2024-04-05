9f7e96df3d2fd804ff245a5aee305327fe9b9700
carolctts <carol.cottas@gmail.com>
2024-04-05 12:01:32 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,11 @@
+# para resolver com índice 13
+
+indice = int(input("Digite um número inteiro: ")) # se for 3
+soma = 0
+k = 0
+
+while k < indice: # k < 3
+	k = k + 1 # k = 1, k = 2, k = 3
+	soma = soma + k # soma = 1, soma = 3, soma = 6
+
+print(soma)
